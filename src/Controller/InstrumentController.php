531759cee8cb8bc64e9b735b53e1f58b0b1f329e
Xavier CHAVAUDREY <chavaudreyxav@hotmail.fr>
2021-11-25 18:06:50 +0100

    controller-result

--- src/Controller/InstrumentController.php
+++ src/Controller/InstrumentController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Model\InstrumentManager;
-use App\
+use App\Service\LastFmApi;
 class InstrumentController extends AbstractController
 {
     public function index(): string
@@ -14,15 +14,16 @@ class InstrumentController extends AbstractController
         return $this->twig->render('Instrument/list.html.twig', ['instruments' => $instruments]);
     }
 
-    public function result(int $id): string
+    public function result(): string
     {
-
+        $tracks = [];
         $instrumentManager = new InstrumentManager();
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $foundIds = array_keys($_POST);
+            $tags = $instrumentManager->selectTagFromInstrument(intval($foundIds[0]));
+            $tracks = LastFmApi::tagGetTopTracks($tags[0]);
         }
-        $tags = $instrumentManager->selectTagFromInstrument($id);
 
-        return $this->twig->render('instrument/results.html.twig', ['instruments' => $instruments]);        
+        return $this->twig->render('instrument/results.html.twig', ['tracks' => $tracks]);        
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -14,5 +14,5 @@ return [
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
     'instrument' => ['InstrumentController', 'index'],
-
+    'instrument/results' => ['InstrumentController', 'results'],
 ];
