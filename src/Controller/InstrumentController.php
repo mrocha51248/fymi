03bd2de656ce7e5e40330664da6a476f8672898e
Xavier CHAVAUDREY <chavaudreyxav@hotmail.fr>
2021-11-25 17:34:58 +0100

    manager

--- src/Controller/InstrumentController.php
+++ src/Controller/InstrumentController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Model\InstrumentManager;
-
+use App\
 class InstrumentController extends AbstractController
 {
     public function index(): string
@@ -12,5 +12,17 @@ class InstrumentController extends AbstractController
         $instruments = $instrumentManager->selectAll('id');
 
         return $this->twig->render('Instrument/list.html.twig', ['instruments' => $instruments]);
+    }
+
+    public function result(int $id): string
+    {
+
+        $instrumentManager = new InstrumentManager();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $foundIds = array_keys($_POST);
+        }
+        $tags = $instrumentManager->selectTagFromInstrument($id);
+
+        return $this->twig->render('instrument/results.html.twig', ['instruments' => $instruments]);        
     }
 }
